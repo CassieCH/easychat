d033c76b5748beaa616c2cf45688a73a58c03a71
CassieCH <87138505@qq.com>
2019-08-09 21:24:45 +0800

    基于websocket的网页聊天室

---
+++ chatroom/src/main/java/entity/User.java
@@ -0,0 +1,39 @@
+package entity;
+
+import lombok.Data;
+
+/**
+ * @Author: yuisama
+ * @Date: 2019-07-30 21:22
+ * @Description:
+ */
+@Data
+public class User {
+    private Integer id;
+    private String userName;
+    private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
